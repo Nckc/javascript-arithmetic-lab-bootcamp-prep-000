3c3134096fd9746c707b7efe9b1fb59bd4621520
Nat C <natcarn@gmail.com>
2018-01-25 05:13:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function preserveDecimal(n) {
   return parseFloat(n)
 }
 function preserveDecimal(n) {
-  return parseFloat('n')
+  return parseFloat('2.222')
 }
 function preserveDecimal(n) {
   return parseInt('n')
